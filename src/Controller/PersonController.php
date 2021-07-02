0e19a1359383b194e4372d7f2ce0507893b8a9f5
AlanBCole <alan.cole@colorado.edu>
2021-07-02 15:51:17 -0600

    added basic template

--- src/Controller/PersonController.php
+++ src/Controller/PersonController.php
@@ -4,37 +4,37 @@ namespace Drupal\person\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Drupal\Core\StreamWrapper\PublicStream;
 
 class PersonController extends ControllerBase {
   public function showPerson($nodeNumber) {
     $person = $this->entityTypeManager()->getStorage('node')->load($nodeNumber);
-    $data = $person->label();
     $personFields = $person->toArray();
 
-    return new JsonResponse(
-      [
-        'nodeNumber' => $nodeNumber,
-        'person' => [ 'type' => $person->bundle(), 'id' => $person->id() ],
-        'title' => $person->label(),
-        'firstName' => $personFields['field_person_first_name'],
-        'lastName' => $personFields['field_person_last_name'],
-        // 'photo' => $personFields['field_person_photo'],
-        'photo' => $this->getTargetIdsFromFieldArray($personFields['field_person_photo'], 'file'),
-        'jobTitle' => $personFields['field_person_job_title'][0]['value'],
-        'jobType' => $this->getTargetIdsFromFieldArray($personFields['field_person_job_type'], 'taxonomy_term'),
-        'department' => $this->getTargetIdsFromFieldArray($personFields['field_person_department'], 'taxonomy_term'),
-        'email' => $personFields['field_person_email'][0]['value'],
-        'phone' => $personFields['field_person_phone'][0]['value'],
-        'address' => $personFields['field_person_address'][0]['value'],
-        'officeHours' => $personFields['field_person_office_hours'][0]['value'],
-        'links' => $personFields['field_person_links'],
-        'bio' => $personFields['body'][0]['value'],
-        'filter1' => $this->getTargetIdsFromFieldArray($personFields['field_person_filter_1'], 'taxonomy_term'),
-        'filter2' => $this->getTargetIdsFromFieldArray($personFields['field_person_filter_2'], 'taxonomy_term'),
-        'filter3' => $this->getTargetIdsFromFieldArray($personFields['field_person_filter_3'], 'taxonomy_term'),
-        'everything' => $personFields
-      ]
-    );
+    $personContent = [
+      '#theme' => 'person_template',
+      '#nodeNumber' => $nodeNumber,
+      '#person' => [ 'type' => $person->bundle(), 'id' => $person->id() ],
+      '#title' => $person->label(),
+      '#firstName' => $personFields['field_person_first_name'],
+      '#lastName' => $personFields['field_person_last_name'],
+      '#photo' => $this->getTargetIdsFromFieldArray($personFields['field_person_photo'], 'file'),
+      '#jobTitle' => $personFields['field_person_job_title'][0]['value'],
+      '#jobType' => $this->getTargetIdsFromFieldArray($personFields['field_person_job_type'], 'taxonomy_term'),
+      '#department' => $this->getTargetIdsFromFieldArray($personFields['field_person_department'], 'taxonomy_term'),
+      '#email' => $personFields['field_person_email'][0]['value'],
+      '#phone' => $personFields['field_person_phone'][0]['value'],
+      '#address' => $personFields['field_person_address'][0]['value'],
+      '#officeHours' => $personFields['field_person_office_hours'][0]['value'],
+      '#links' => $personFields['field_person_links'],
+      '#bio' => $personFields['body'][0]['value'],
+      '#filter1' => $this->getTargetIdsFromFieldArray($personFields['field_person_filter_1'], 'taxonomy_term'),
+      '#filter2' => $this->getTargetIdsFromFieldArray($personFields['field_person_filter_2'], 'taxonomy_term'),
+      '#filter3' => $this->getTargetIdsFromFieldArray($personFields['field_person_filter_3'], 'taxonomy_term'),
+    ];
+
+    return $personContent;
+    // return new JsonResponse($personContent);
   }
 
   private function getTargetIdsFromFieldArray(array $fieldArray, $entityType) {
@@ -51,8 +51,10 @@ class PersonController extends ControllerBase {
             break;
 
           case 'file':
+            $fileLocation = explode('://', $fieldData->getTypedData()->get('uri')->getString())[1];
+            $baseFilepath = PublicStream::basePath();
             $desiredFieldData = [
-              // 'info' => $fieldData->getF,
+              'uri' => implode('/', ['', $baseFilepath, $fileLocation]),
               'alt' => $field['alt'],
               'targetId' => $field['target_id'],
               'bundle' => $fieldData->bundle()
